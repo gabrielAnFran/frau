fc9e3c11299e5b7bef03ace8d8a80a33b2079c9a
gabrielAnFran <antunes.f.gabriel@gmail.com>
2024-07-10 18:35:24 -0300

    a

--- internal/infra/db/expenses-repository_test.go
+++ internal/infra/db/expenses-repository_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestAddRecurrentExpenses(t *testing.T) {
-	// funcUsingEnvVar()
+	// funcUsingEnvVar()///
 
 	// // Create a new repository
 	// repo := NewRecurrentExpensesRepository()
